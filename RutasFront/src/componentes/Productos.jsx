9190a66046d4461181a4475c98aa3123af89b5f2
erickslk <erick.osorto@unitec.edu>
2023-10-23 22:00:59 -0600

    main

---
+++ RutasFront/src/componentes/Productos.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Col } from 'react-bootstrap'
+
+export const Producto = () => {
+  return (
+    <>
+
+   <div className='container m-5'>
+    <div className='row text-align-center'>
+    <h1>Productos</h1>
+    </div>
+   
+    </div> 
+
+<div className='container'>
+
+<div className="card col-lg-6" >
+ <img className="card-img-top" src="" alt="" />
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+    </>
+  )
+}
